3815862c0b70b21fcd116e504bcd029fb5e3e86b
YEJIN-JEONG <wls7367@naver.com>
2021-09-15 20:41:47 +0900

    파이썬 대소문자 변환

---
+++ String.py
@@ -0,0 +1,10 @@
+#문자열 msg = “Data Structures in Python”을 선언하고,
+# 먼저 이 문자열을 그대로 출력하고,
+# 다음 모두 대문자로 바꿔 출력하고,
+# 다음 모두 소문자로 바꿔 출력하고,
+# 마지막으로 소문자는 대문자로 대문자는 소문자로 바꿔 출력한다.
+msg = 'Data Structures in Python'
+print(msg)
+print(msg.upper())
+print(msg.lower())
+print(msg.swapcase())
