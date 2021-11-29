5fc688f4428a251a827bc6fc191ff37a62b2ac4c
YEJIN-JEONG <wls7367@naver.com>
2021-11-29 16:58:30 +0900

    파이썬 dataTypeQueue.py

---
+++ dataTypeQueue.py
@@ -0,0 +1,74 @@
+import re
+MAX_SIZE = 3
+class CircularQueue :
+    def __init__( self ) :
+        self.items = [None] * MAX_SIZE
+        self.front = 0
+        self.rear = 0
+        self.count=0
+        self.maxSize=MAX_SIZE
+
+    def isEmpty( self ) :
+        #return self.front == self.rear
+        return self.count==0
+    def isFull( self ) :
+        return self.front == (self.rear+1)%self.maxSize
+    def clear( self ) :
+        self.front = 0
+        self.rear = 0
+        self.count=0
+        self.maxSize=MAX_SIZE
+
+    def enqueue( self, item ):
+        if self.isFull():
+            self.resize()
+
+        self.rear = (self.rear+1)% self.maxSize
+        self.items[self.rear] = item
+        self.count+=1
+
+    def dequeue( self ):
+        if not self.isEmpty():
+            self.front = (self.front+1)% self.maxSize
+            self.count-=1
+            return self.items[self.front]
+
+    def resize(self):
+        newItems=[None]*2*self.maxSize
+        scan=(self.front+1)%self.maxSize
+        for i in range(self.count):
+            newItems[i+1]=self.items[scan]
+            scan=(scan+1)%self.maxSize
+        self.items=newItems
+        self.front=0
+        self.rear=self.count
+        self.maxSize=2*self.maxSize
+
+    def peek( self ):
+        if not self.isEmpty():
+            return self.items[(self.front + 1) % self.maxSize]
+
+    def size( self ) :
+       return (self.rear - self.front + self.maxSize) % self.maxSize
+
+    def print( self ):
+        out = []
+        if self.front < self.rear :
+            out = self.items[self.front+1:self.rear+1]
+        elif self.front>self.rear:
+            out = self.items[self.front+1:self.maxSize] \
+                + self.items[0:self.rear+1]
+        print(out)
+
+def mydataType():
+    q=CircularQueue()
+    strq=CircularQueue()
+    intq=CircularQueue()
+    floatq=CircularQueue
+    f=open("p5input.txt","r")
+    lines =f.read().split()
+    for line in lines:
+        q.enqueue(line)
+    f.close()
+
+mydataType()
